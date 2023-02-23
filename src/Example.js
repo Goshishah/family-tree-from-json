6e3bacc4831fd1a103a129491d1e255a00e88fd7
shujaat ali <shujaat.mcs@gmail.com>
2023-02-23 11:52:50 +0500

    update style

--- src/Example.js
+++ src/Example.js
@@ -2,6 +2,7 @@ import React from "react";
 import Xarrow, { useXarrow, Xwrapper } from "react-xarrows";
 import Draggable from "react-draggable";
 import data from "./data.json";
+import { getData } from "./getTree";
 
 const DraggableBox = ({ id, item }) => {
   const updateXarrow = useXarrow();
@@ -10,13 +11,17 @@ const DraggableBox = ({ id, item }) => {
       <span
         id={id}
         style={{
-          backgroundColor: "green",
-          color: "white",
-          padding: "10px",
+          background:
+            "radial-gradient(circle, rgba(2,76,12,1) 0%, rgba(4,119,2,1) 0%, rgba(0,62,16,1) 100%)",
+          fontSize: "24px",
+          color: "#fdce1f",
+          padding: "20px",
           margin: "50px",
-          border: "5px solid yellow",
+          border: "5px solid #fdce1f",
           display: "inline-block",
-          borderRadius: "10px"
+          // borderRadius: "10px",
+
+          borderRadius: "50%"
         }}
       >
         {item}
@@ -29,8 +34,11 @@ export function V2Example() {
   return (
     <div>
       <Xwrapper>
+        {/* <DraggableBox id={"elem-grand"} item={""} />
+      <DraggableBox id={"elem-grand"} item={""} />
+      <DraggableBox id={"elem-grand"} item={""} /> */}
         {data.map((item, index) => (
-          <DraggableBox id={"elem" + index} item={item} />
+          <DraggableBox id={"elem" + index} item={item.replace("بن", "")} />
         ))}
         {/* <Xarrow start={"elem" + 0} end={"elem" + 1} />
         <Xarrow start={"elem" + 1} end={"elem" + 2} />
